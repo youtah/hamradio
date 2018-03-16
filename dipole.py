54bdd6403ff9e8e789394294dda7da77de36c3f7
Jeff McGrath <jeff@youtah.com>
2018-03-16 10:58:18 -0600

    Updated to Support Python 3
    Made the frequency a required arg.
    Updated the print statements to work with python 3

--- dipole.py
+++ dipole.py
@@ -6,10 +6,10 @@ from fractions import Fraction
 
 def main():
     args = makeargs()
-    print "Frequency: "+str(args.megahertz)
-    print "Velocity: "+str(args.velocity)
+    print("Frequency: "+str(args.megahertz))
+    print("Velocity: "+str(args.velocity))
 
-    feet = (((300/args.megahertz)/4)*(args.velocity*0.01)*3.28084)
+    feet = ((300/args.megahertz)/4)*(args.velocity*0.01)*3.28084
     inches = (feet-int(math.floor(feet)))*10
     fraction = Fraction(round(inches-math.floor(inches),2)).limit_denominator()
     if (fraction is not None) and (fraction != 0):
@@ -20,11 +20,11 @@ def main():
     inches = int(math.floor(inches))
     if inches > 1:
         inches_string = "es"
-    print str(feet)+" feet, "+str(inches)+" "+str(frac_string)+" inch"+str(inches_string)
+    print(str(feet)+" feet, "+str(inches)+" "+str(frac_string)+" inch"+str(inches_string))
 
 def makeargs():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--megahertz', type=float, help='Interger or floating point in megahertz. Example: 14.225.')
+    parser.add_argument('-m', '--megahertz', required=True, type=float, help='Interger or floating point in megahertz. Example: 14.225.')
     parser.add_argument('-v', '--velocity', type=int, default=95, help='Integer: This is the Velocity Factor, in a percentage. Example: 95 for 95 percent.')
     args = parser.parse_args()
     return args
